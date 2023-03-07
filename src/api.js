0aef8f320053c5feed261fd5ae7daaf9599ff60d
Jonathan Wagner <40695629+w0kyj@users.noreply.github.com>
2023-03-07 08:33:21 -0700

    Update api.js
    Changing to public access for the APIs

--- src/api.js
+++ src/api.js
@@ -5,9 +5,6 @@ const axios = require("axios");
 
 const api = new axios.create({
   baseURL: process.env.VUE_APP_API_URL,
-  headers: {
-    Authorization: "Bearer " + process.env.VUE_APP_API_TOKEN,
-  },
 });
 
 module.exports = api;
